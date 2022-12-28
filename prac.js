1e5382c1894d05ef31a26bfc0562da9008127da5
PENN JR <pennjunior680@gmail.com>
2022-12-28 18:54:28 +0100

    learning how to take decisions with the if/else statement in javascript

--- prac.js
+++ prac.js
@@ -3,10 +3,33 @@
 // x++
 // console.log(x)
 
-const firstName = "malone"
-const job = 'Web Developer'
-const birthYear = 2000
-const year = 2022
 
-const malone = " I'm " + firstName + ' ,a ' + (year -  birthYear) +  ' years old '  + job + '!'
-console.log(malone)
+//strings and Template Literals
+// const firstName = "malone"
+// const job = 'Web Developer'
+// const birthYear = 2000
+// const year = 2022
+
+// // const malone = " I'm " + firstName + ' ,a ' + (year -  birthYear) +  ' years old '  + job + '!'
+// // console.log(malone)
+
+// const maloneNew = `I'm ${firstName} ,a ${year - birthYear} year old ${job}`
+// console.log(maloneNew)
+
+// const age = 14
+// if (age >= 18) {
+//     console.log(`malone can start his driving license🚗`)
+// } else {
+//     yearsLeft = 18 - age
+//     console.log(`Malone is too young. please wait another ${yearsLeft} years 😉`)
+// }
+
+const birthYear = 1999
+let century
+
+if(birthYear <= 2000) {
+    century = 20
+} else {
+    century = 21
+}
+console.log(`was born in the ${century} century`)
