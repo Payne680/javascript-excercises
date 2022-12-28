e02ea4394c8b5b9dc77f61a83923d3b06e4a9a3b
PENN JR <pennjunior680@gmail.com>
2022-12-28 17:59:52 +0100

    Doing string and template literal exercises

---
+++ prac.js
@@ -0,0 +1,12 @@
+// let x = 10 / 5;
+// x += 10
+// x++
+// console.log(x)
+
+const firstName = "malone"
+const job = 'Web Developer'
+const birthYear = 2000
+const year = 2022
+
+const malone = " I'm " + firstName + ' ,a ' + (year -  birthYear) +  ' years old '  + job + '!'
+console.log(malone)
