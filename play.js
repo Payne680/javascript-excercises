d18ee66f3010ef3d788d96089bb9e4533c202feb
PENN JR <pennjunior680@gmail.com>
2023-04-06 20:36:17 +0100

    Solving some algorithms

--- play.js
+++ play.js
@@ -1,17 +1,31 @@
-/*
-write a program that uses console.log to print all the number from 1 to 100 with two 
- */
+// /*
+// write a program that uses console.log to print all the number from 1 to 100 with two 
+//  */
 
-let num =10;
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < i; j++) {
-        process.stdout.write('#')
+// let num =10;
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < i; j++) {
+//         process.stdout.write('#')
+//     }
+//     console.log(' ');
+// }
+
+// let text = '#'
+// for (let i = 0; i < Array.length; i++) {
+//     console.log(text);
+//     text += '#';
+// }
+
+function compareTripplet(a, b) {
+    let alice = 0;
+    let bob = 0;
+    for(let i = 0; i < a.length; i++) {
+        if(a[i]>b[i]){
+            alice++;
+        }
+        else if(b[i]>a[i]){
+            bob++
+        }
     }
-    console.log(' ');
-}
-
-let text = '#'
-for (let i = 0; i < Array.length; i++) {
-    console.log(text);
-    text += '#';
+    return[alice, bob]
 }
